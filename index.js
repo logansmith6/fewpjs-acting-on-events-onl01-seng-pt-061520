5706ed4d5e9040e34267f5eb3aeadcfe06e1f6bf
Logan Smith <logsmith06@gmail.com>
2020-12-15 19:33:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ document.addEventListener("keydown", function(e) {
     dodger.style.left = `${left - 1}px`;
   }
 });
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
