0c112d879e0edca0304c7aad2667fac80c9e9f9b
Logan Smith <logsmith06@gmail.com>
2020-12-15 19:31:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Your code here
 
 document.addEventListener("keydown", function(e) {
-  console.log(e.key);
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+  }
 });
